f6574253abbf899df1d4df239314f081aab7b796
MiltonValdez <ferrufinovaldezbruno@pedropoveda.edu.bo>
2026-05-08 11:28:49 -0400

    Teban

---
+++ eleminarProductos.php
@@ -0,0 +1,34 @@
+<?php
+$usuario = "root";
+$contraseña = "";     
+$direccion = "localhost";
+$baseDeDatos = "MYMS";   
+ 
+$conn = new mysqli ($usuario,$contraseña,$direccion,$baseDeDatos);
+$sql="DELETE FROM Productos WHERE Nombre=".$_GET['Nombre'];
+$conn->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Eliminar Registro</title>
+  <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&display=swap" rel="stylesheet">
+
+</head>
+<body>
+  <div class="mensaje">
+    <h2>Eliminación de Registro</h2>
+    <p>
+      <?php
+        echo "El registro ha sido eliminado.";
+        $conn->close(); 
+      ?>
+    </p>
+    <a href="menu.html">Volver al menú</a>
+  </div>
+</body>
+</html>
+    
