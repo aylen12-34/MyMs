9c37f42b3c174e224a60b000a3b56ba3c1c0d7d3
MiltonValdez <hagointrosgratis12@gmail.com>
2026-05-08 11:45:23 -0400

    tebandida

--- registroProductos.php
+++ registroProductos.php
@@ -1,5 +1,15 @@
 <?php
-require "bdProductos.php";
+$usuario = "root";
+$contraseña = "";     
+$direccion = "localhost";
+$baseDeDatos = "MYMS";   
+ 
+$conexion=new mysqli($direccion, $usuario, $contraseña, $baseDeDatos);
+if ($conexion->connect_error) {
+    
+    echo "No se ha podido conectar a la base de datos";
+}
+
 $Nombre=$_POST['Nombre'];
 $Descripcion=$_POST['Descripcion'];
 $Precio=$_POST['Precio'];
