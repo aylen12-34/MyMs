ca7e5e859d6d0ff0ce0cce2d1ad9e2c1e32223ae
MiltonValdez <hagointrosgratis12@gmail.com>
2026-05-08 11:49:33 -0400

    Update formUpdateProductos.php

--- formUpdateProductos.php
+++ formUpdateProductos.php
@@ -8,17 +8,13 @@
 <body>
     <form action="updateEditarProductos.php" method="post">
         <label for="Nombre">Nombre:</label>
-        <input type="text" id="Nombre" name="Nombre" required>
-        <br>
+        <input type="text" id="Nombre" name="Nombre" required>  <br>  <br>
         <label for="Direccion">Descripción:</label>
-        <input type="text" id="Descripcion" name="Descripcion" required>
-        <br>
+        <input type="text" id="Descripcion" name="Descripcion" required>  <br>  <br>
         <label for="Precio">Precio:</label>
-        <input type="text" id="Precio" name="Precio" required>
-        <br>
-                <label for="Stock">Stock:</label>
-        <input type="text" id="Stock" name="Stock" required>
-        <br>
+        <input type="text" id="Precio" name="Precio" required>  <br>  <br>
+        <label for="Stock">Stock:</label>
+        <input type="text" id="Stock" name="Stock" required>  <br>  <br>
         <input type="submit" value="Editar">
     </form>
 </body>
