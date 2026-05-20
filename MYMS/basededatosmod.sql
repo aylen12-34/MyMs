154b9c51c260c62f9a3bf0d7869096bc82874776
aylen12-34 <aylenfloresguarachi@gmail.com>
2026-05-20 08:09:51 -0400

    basededatosmodificado

--- MYMS/basededatosmod.sql
+++ MYMS/basededatosmod.sql
@@ -1,62 +1,46 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 15-05-2026 a las 19:08:46
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- MySQL Workbench Forward Engineering
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema MYMS
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema MYMS
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `MYMS` DEFAULT CHARACTER SET utf8 ;
+USE `MYMS` ;
+
+-- -----------------------------------------------------
+-- Table `MYMS`.`Productos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `MYMS`.`Productos` (
+  `Codigo` INT NOT NULL AUTO_INCREMENT,
+  `Nombre` VARCHAR(45) NULL,
+  `Descripcion` VARCHAR(45) NULL,
+  `Precio` VARCHAR(45) NULL,
+  `Stock` VARCHAR(45) NULL,
+  PRIMARY KEY (`Codigo`))
+ENGINE = InnoDB;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- -----------------------------------------------------
+-- Table `MYMS`.`Usuarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `MYMS`.`Usuarios` (
+  `CI` INT NOT NULL AUTO_INCREMENT,
+  `Nombre` VARCHAR(45) NULL,
+  `Direccion` VARCHAR(45) NULL,
+  `Celular` VARCHAR(45) NULL,
+  `Rol` VARCHAR(45) NULL,
+  `Estado` VARCHAR(45) NULL,
+  PRIMARY KEY (`CI`))
+ENGINE = InnoDB;
 
---
--- Base de datos: `myms`
---
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `productos`
---
-
-CREATE TABLE `productos` (
-  `Codigo` int(11) NOT NULL,
-  `Nombrep` varchar(45) DEFAULT NULL,
-  `Descripcion` varchar(45) DEFAULT NULL,
-  `Precio` varchar(45) DEFAULT NULL,
-  `Stock` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Volcado de datos para la tabla `productos`
---
-
-INSERT INTO `productos` (`Codigo`, `Nombrep`, `Descripcion`, `Precio`, `Stock`) VALUES
-(9, 'carneasada', 'tucola', '67465435', '69777'),
-(10, 'fhdh', 'fhdhdfh', '6565', '646'),
-(11, 'fhdh', 'fhdhdfh', '6565', '646'),
-(12, 'fhdhfew', 'fhdhdfh', '6565', '646'),
-(13, 'fhdhfew', 'fhdhdfh', '6565', '646');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`Codigo`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
