640743084f9731bf0ee8427acfa6ebfa9e63f811
LeonelVillarroelMendoza <hagointrosgratis12@gmail.com>
2026-08-12 15:28:39 -0400

    FORM ventas corrido 100%$ da

--- Ventas/registroventas.php
+++ Ventas/registroventas.php
@@ -1,14 +1,16 @@
 <?php
 require "bdVentas.php";
-$Pedidos_ID=$_POST['Pedidos_ID'];
-$Costototal=$_POST['Costototal'];
-$Estado=$_POST['Estado'];
-$Metodo=$_POST['Metodo'];
- 
-$sql="INSERT INTO Ventas (Pedidos_ID, Costototal, Estado, Metodo) VALUES ('$Pedidos_ID', '$Costototal', '$Estado', '$Metodo')";
+
+$Pedidos_ID = $_POST['ID'];
+$Costototal = $_POST['Costototal'];
+$Estado = $_POST['Estado'];
+$Metodo = $_POST['Metodo'];
+
+$sql = "INSERT INTO Ventas (Pedidos_ID, Costototal, Estado, Metodo) VALUES ('$Pedidos_ID', '$Costototal', '$Estado', '$Metodo')";
+
 if ($conexion->query($sql) === TRUE) {
-     echo "registrado correctamente";
-    }else{
-     echo "Hubo un error";
+    echo "Registrado correctamente";
+} else {
+    echo "Hubo un error: " . $conexion->error;
 }
 ?>
